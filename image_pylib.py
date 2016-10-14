411cfff14f7c0ae58a51257fd9191d76d7a6ce16
Shiyu Huang <huangsy13@gmail.com>
2016-10-14 15:24:15 +0800

    update

--- image_pylib.py
+++ image_pylib.py
@@ -33,11 +33,26 @@ class BBX:
 class IMGLIB:
     def __init__(self):
         pass
+    def setBBXs(self,bboxs,name0):
+        self.bbxs = []
+        for bbox in bboxs:
+            bbx = BBX()
+            bbx.name = name0
+            bbx.x = int(bbox[0])
+            bbx.y = int(bbox[1])
+            bbx.w = int(bbox[2])
+            bbx.h = int(bbox[3])
+            bbx.score = bbox[4]
+            self.bbxs.append(bbx)
+
+    def showBBXs(self):
+        self.drawBox()
+        self.img.show()
     def savaBBXs(self,fileName):
         f = open(fileName,'w')
         f.write('% bbGt version=3\n')
         for bbx in self.bbxs:
-            f.write('%s %d %d %d %d 0 0 0 0 0 0 0'%(bbx.name,bbx.x,bbx.y,bbx.w,bbx.h))
+            f.write('%s %d %d %d %d 0 0 0 0 0 0 0\n'%(bbx.name,bbx.x,bbx.y,bbx.w,bbx.h))
         f.close()
 
     def drawOneBox(self,bbx):
@@ -59,9 +74,7 @@ class IMGLIB:
             self.drawOneBox(bbx)
 
     def read_img(self, fileName):
-        
         self.img = Image.open(fileName)
-
 
     def read_ano(self, fileName):
     
@@ -79,27 +92,27 @@ class IMGLIB:
         for bbx in self.bbxs:
             bbx.resize(r,x_d,y_d)
 
-    def resize(self, width, height):
+    def resize(self, width, height,scale = 1.0):
         o_width, o_height = self.img.size
+        t_width = int(width * scale)
+        t_height= int(height* scale)
+
         o_ratio = o_width/float(o_height)
         n_ratio = width/float(height)
         
         if o_ratio > n_ratio:
-            re_ration = width/float(o_width)
+            re_ration = t_width/float(o_width)
             a_height = int(re_ration*o_height)
-            a_width = width
-            self.x_d = 0
-            self.y_d = random.randint(0, abs(a_height-height) )
+            a_width = t_width
             self.img = self.img.resize((a_width,a_height),Image.ANTIALIAS)
         else:
-            re_ration = height/float(o_height)
+            re_ration = t_height/float(o_height)
             a_width = int(re_ration*o_width)
-            a_height = height
-            self.y_d = 0
-            self.x_d = random.randint(0, abs(a_width-width) )
+            a_height = t_height
             self.img = self.img.resize( (a_width,a_height),Image.ANTIALIAS)
 
-        
+        self.x_d = random.randint(0, abs(a_width-width) )
+        self.y_d = random.randint(0, abs(a_height-height) )
         imgNew = Image.new("RGB", (width, height), "black")
 
         box = (0,0,a_width,a_height) 
@@ -125,7 +138,7 @@ if __name__ == '__main__':
 
     imglib.read_img(imageName)
     imglib.read_ano(anoName)
-    imglib.resize(imgWidth,imgHeight)
+    imglib.resize(imgWidth,imgHeight,0.85)
     imglib.drawBox()
     imglib.save_img(saveImgName)
     imglib.savaBBXs(saveAnoName)
